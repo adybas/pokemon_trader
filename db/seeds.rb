1a33a5a8ee68fb45f566185ab8489d12852330a8
Anna Dybas <a.m.dybas@gmail.com>
2020-01-21 16:22:47 -0500

    seeding data through gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'pokemon_tcg_sdk'
+require 'pry'
+cards = Pokemon::Card.all
+
+cards.map do |pokemon|
+    name = pokemon.name
+    image = pokemon.image_url
+    hp = pokemon.hp
+    national_pokedex_number = pokemon.national_pokedex_number
+    rarity = pokemon.rarity
+    evolves_from = pokemon.evolves_from
+    Card.find_or_create_by(name: name, image_url: image, hp: hp, national_pokedex_number: national_pokedex_number, rarity: rarity, evolves_from: evolves_from)
+end
