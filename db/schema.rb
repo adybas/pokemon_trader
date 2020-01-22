0e6c6d35999019a8d13b273f60536dd052b37179
Anna Dybas <a.m.dybas@gmail.com>
2020-01-22 10:38:57 -0500

    adding sender_id and receiver_id to trade schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_150758) do
+ActiveRecord::Schema.define(version: 2020_01_22_153713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2020_01_22_150758) do
     t.string "trade_status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "sender_id"
+    t.integer "receiver_id"
   end
 
   create_table "trainers", force: :cascade do |t|
