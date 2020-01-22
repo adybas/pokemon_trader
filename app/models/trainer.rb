c682b570ea6d8aa7ff7f4362d1097bed23f8b796
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-22 12:18:07 -0500

    Merged conflicts

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -3,7 +3,9 @@ class Trainer < ApplicationRecord
     has_many :card_binders 
     has_many :cards, through: :card_binders
 
-<<<<<<< HEAD
+    has_many :trades
+    has_many :senders, through: :trades
+    has_many :receivers, through: :trades
 
     
     def open_a_pack 
@@ -33,9 +35,5 @@ class Trainer < ApplicationRecord
     end 
 
     private 
-=======
-    has_many :trades
-    has_many :senders, through: :trades
-    has_many :receivers, through: :trades
->>>>>>> 0e6c6d35999019a8d13b273f60536dd052b37179
+
 end
